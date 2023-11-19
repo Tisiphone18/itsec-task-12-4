f6e861b03de3a86bb613d3c2775d12aa1ea51d79
lenaholtmannspoetter <lena@holtmannspoetter.de>
2023-11-19 19:30:25 +0100

    Initial commit

---
+++ pwn-students.py
@@ -0,0 +1,87 @@
+import binascii
+import socket
+
+# If you have done that, copy over a hexlified message + IV over to this script (replacing the zeros)
+iv = binascii.unhexlify("507dac229ba1ce86991054cfa76c102f")
+msg = binascii.unhexlify(
+    "fa56b5c113b6c4c008e52aa62d8fdbc31c71c7d4b2017652fe22b35e0a0f270887794a12375e9ecfe1e8fd54027769140abaa5445c59b251a22fb2ad36be6896")
+
+
+def read_until(s, token):
+    """Reads from socket `s` until a string `token` is found in the response of the server"""
+    buf = b""
+    while True:
+        data = s.recv(2048)
+        buf += data
+        if not data or token in buf:
+            return buf
+
+def xor_block_with_list(block, list):
+    """XORs a block with a list of bytes"""
+    return bytes([(block[i] ^ (list[i])) for i in range(len(block))])
+
+
+def char_entschluesseln(vektor, blocks, entschluesselter_block, stelle):
+
+    padding = ([0x0] * (15 - stelle)) + ([stelle + 1] * (stelle + 1)) # [0,0,...,(i+1),(i+1)]
+    entschlüsselte_Werte_mit_padding = [padding[i] ^ entschluesselter_block[i] for i in range(len(padding))]
+
+    blocks_kopie = blocks[:]
+    vector_kopie = vektor
+
+    # alle chars durchlaufen
+    for char in range(0, 256):
+        if char != stelle + 1: # sonst Sonderfall !
+
+            #xor Wert an Pos stelle mit char
+            arr = [0x0] * (15 - stelle) + [char] + [0x0] * stelle
+            angriffsvektor = [entschlüsselte_Werte_mit_padding[i] ^ arr[i] for i in range(len(entschlüsselte_Werte_mit_padding))]
+
+            # xor den block (or den vector in der letzten Runde) mit dem angriffsvektor
+            if len(blocks) == 1:
+                #Vector mit angriffsvektor xorn
+                vector_kopie = xor_block_with_list(vektor, angriffsvektor)
+            else:
+                # Forgängerblock mit angriffsvektor xorn
+                blocks_kopie[-2] = xor_block_with_list(blocks[-2], angriffsvektor)
+
+            #Nachricht zusammenfügen !
+            ganzer_Block = ''.join('{:02x}'.format(byte) for byte in b''.join(blocks)).encode() + b'\n'
+
+            s = socket.socket()
+            s.connect(("itsec.sec.in.tum.de", 7023))
+            start = read_until(s, b"Do you")
+            s.send(binascii.hexlify(vector_kopie) + b"\n")
+            s.send(ganzer_Block)
+            response = read_until(s, b"\n")
+
+            if b"OK" in response: #Wert speichern und aus Schleife
+                entschluesselter_block[15 - stelle] = char
+                return char
+
+    # wenn kein valider Char existiert, dann Sonderfall
+    char = stelle + 1
+    entschluesselter_block[15 - stelle] = char
+    return char
+
+entschluesselter_Text = []
+
+blocks = [msg[i:i + 16] for i in range(0, len(msg), 16)]
+
+# Blöcke entschlüsseln
+for b in range(0, 4):
+
+    entschluesselter_block = [0x0 for _ in range(16)]
+
+    # jedes Byte im Block entschlüsseln
+    for stelle in range(0, 16):
+        char = char_entschluesseln(iv, blocks, entschluesselter_block, stelle)
+        entschluesselter_Text.insert(0, char)
+        print(char)
+
+    # letzten Block entfernen um weiter zumachen mit 16 byte kürzerer Nachricht
+    del blocks[-1]
+
+# entschlüsselte Flag:
+print(f"\nMeine Flag ist: ", bytes(entschluesselter_Text))
+
---
+++ server.py
@@ -0,0 +1,78 @@
+from Crypto.Cipher import AES
+import binascii
+import os
+import sys
+import asyncio
+import subprocess
+
+
+if os.path.exists("secret-key.bin"):
+    with open("secret-key.bin", "rb") as keyfile:
+        key = keyfile.read()
+else:
+    key = os.urandom(16)
+    with open("secret-key.bin", "wb") as keyfile:
+        keyfile.write(key)
+
+
+class PaddingError(Exception):
+    pass
+
+def pad_message(msg):
+    val = 16 - (len(msg) % 16)
+    return msg + "{}".format(chr(val)).encode()*val
+
+def unpad_message(msg):
+    val = msg[-1]
+    if not all(x==val for x in msg[-val:]):
+        raise PaddingError()
+    msg = msg[:-val]
+    return msg
+
+mhex = lambda x: binascii.hexlify(x).decode()
+
+async def handle_request(reader, writer):
+    print("New connection")
+    iv = os.urandom(16)
+    secret_msg = b"This is your flag: " + subprocess.check_output("/bin/flag") + b"\n"
+    padded_msg = pad_message(secret_msg)
+
+    my_aes = AES.new(key, AES.MODE_CBC, iv)
+    crypted_msg = my_aes.encrypt(padded_msg)
+
+    writer.write("I have an encrypted message for you:\n{} (IV was {})\n\n".format(mhex(crypted_msg), mhex(iv)).encode())
+    writer.write(b"Do you also have an encrypted message for me?!\nIf so, please enter IV and the message seperated by newlines now! (plz give hexlified stuff)\n")
+    await writer.drain()
+
+    iv = await reader.readline()
+    msg = await reader.readline()
+
+    try:
+        iv = binascii.unhexlify(iv.strip())
+        msg = binascii.unhexlify(msg.strip())
+    except binascii.Error:
+        writer.write(b"Couldn't unhexlify your stuff... :(\n")
+    else:
+        try:
+            my_aes = AES.new(key, AES.MODE_CBC, iv)
+            decoded = my_aes.decrypt(msg)
+            print(binascii.hexlify(decoded))
+            decoded = unpad_message(decoded)
+            writer.write(b"OK!\n")
+        except PaddingError:
+            writer.write(b"Bad padding :(\n")
+        except ValueError as e:
+            writer.write("Some other error: {}\n".format(e).encode())
+
+    await writer.drain()
+    writer.close()
+
+async def run_server():
+    server = await asyncio.start_server(handle_request, "0.0.0.0", 1024)
+    ip, port = server.sockets[0].getsockname()
+    print(f"Serving on {ip}:{port}")
+    
+    await server.serve_forever()
+
+if __name__ == "__main__":
+    asyncio.run(run_server())
