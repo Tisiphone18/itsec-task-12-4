602adde0340fd45fcea2bc66179d44cd722adde0
lenaholtmannspoetter <lena@holtmannspoetter.de>
2023-11-19 19:53:01 +0100

    print

--- pwn-students.py
+++ pwn-students.py
@@ -1,10 +1,8 @@
 import binascii
 import socket
 
-# If you have done that, copy over a hexlified message + IV over to this script (replacing the zeros)
 iv = binascii.unhexlify("507dac229ba1ce86991054cfa76c102f")
-msg = binascii.unhexlify(
-    "fa56b5c113b6c4c008e52aa62d8fdbc31c71c7d4b2017652fe22b35e0a0f270887794a12375e9ecfe1e8fd54027769140abaa5445c59b251a22fb2ad36be6896")
+msg = binascii.unhexlify("fa56b5c113b6c4c008e52aa62d8fdbc31c71c7d4b2017652fe22b35e0a0f270887794a12375e9ecfe1e8fd54027769140abaa5445c59b251a22fb2ad36be6896")
 
 
 def read_until(s, token):
@@ -16,30 +14,6 @@ def read_until(s, token):
         if not data or token in buf:
             return buf
 
-
-def check_padding(iv, blocks):
-    """Checks if the padding is valid by sending the encrypted message to the padding oracle"""
-    s = socket.socket()
-    s.connect(("itsec.sec.in.tum.de", 7023))
-
-    start = read_until(s, b"Do you")
-    s.send(binascii.hexlify(iv) + b"\n")
-    s.send(binascii.hexlify(b''.join(blocks)) + b"\n")
-
-    response = read_until(s, b"\n")
-    return b"OK" in response
-
-
-def xor_lists(a, b):
-    """XORs two lists of bytes"""
-    return [a[i] ^ b[i] for i in range(len(a))]
-
-
-def xor_block_with_list(block, list):
-    """XORs a block with a list of bytes"""
-    return bytes([(block[i] ^ (list[i])) for i in range(len(block))])
-
-
 def decrypt_char(iv, blocks, decrypted_values, i):
     """Decrypts a single byte"""
     blocks_copy = blocks.copy()
@@ -49,7 +23,7 @@ def decrypt_char(iv, blocks, decrypted_values, i):
     padding = ([0x0] * (15 - i)) + ([i + 1] * (i + 1))
 
     # xor padding with the previous decrypted values
-    decripted_values_with_padding = xor_lists(padding, decrypted_values)
+    decripted_values_with_padding = [padding[i] ^ decrypted_values[i] for i in range(len(padding))]
 
     # test padding for the i-th byte in the block
     for n in range(0, 256):
@@ -58,16 +32,27 @@ def decrypt_char(iv, blocks, decrypted_values, i):
             continue
 
         # value to be XORed with the block
-        evil = xor_lists(decripted_values_with_padding, [0x0] * (15 - i) + [n] + [0x0] * i)
+        aux = [0x0] * (15 - i) + [n] + [0x0] * i
+        evil = [decripted_values_with_padding[i] ^ aux[i] for i in range(len(decripted_values_with_padding))]
 
         # xor the block (or the iv) with the evil value
-        if (len(blocks) > 1):
-            blocks_copy[-2] = xor_block_with_list(blocks[-2], evil)
+        if (len(blocks) == 1):
+            aux2 = bytes([(iv[i] ^ (evil[i])) for i in range(len(iv))])
+            iv_copy = aux2
         else:
-            iv_copy = xor_block_with_list(iv, evil)
+            aux2 = bytes([(blocks[-2][i] ^ (evil[i])) for i in range(len(blocks[-2]))])
+            blocks_copy[-2] = aux2
 
         # check if the padding is valid
-        if check_padding(iv_copy, blocks_copy):
+
+        s = socket.socket()
+        s.connect(("itsec.sec.in.tum.de", 7023))
+
+        start = read_until(s, b"Do you")
+        s.send(binascii.hexlify(iv_copy) + b"\n")
+        s.send(binascii.hexlify(b''.join(blocks_copy)) + b"\n")
+        response = read_until(s, b"\n")
+        if b"OK" in response:
             decrypted_values[15 - i] = n
             return n
 
@@ -86,9 +71,6 @@ def decypt_block(iv, blocks, decrypted):
         decrypted.append(n)
         print(str(bytes(decrypted[::-1]))[2:-1])
 
-
-# The server allows you to process a single message with each connection.
-# Connect multiple times to decrypt the (IV, msg) pair above byte by byte.
 def main():
     decrypted = []
 
@@ -98,8 +80,8 @@ def main():
     # decrypt blocks
     for i in range(len(blocks)):
         decypt_block(iv, blocks, decrypted)
-        # remove blocks after they are fully decripted
-        blocks.pop()
+        # block entfernen nachdem voll entschlüsselt
+        del blocks[-1]
 
     # print the decrypted message
     print(f"\nDecrypted message: {bytes(decrypted[::-1])}")
