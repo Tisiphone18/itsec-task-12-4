fe1c3dc89c3f247553ec3343496367cfc345d422
lenaholtmannspoetter <lena@holtmannspoetter.de>
2023-11-20 14:12:00 +0100

    print

--- pwn-students.py
+++ pwn-students.py
@@ -3,8 +3,6 @@ import socket
 
 iv = binascii.unhexlify("507dac229ba1ce86991054cfa76c102f")
 msg = binascii.unhexlify("fa56b5c113b6c4c008e52aa62d8fdbc31c71c7d4b2017652fe22b35e0a0f270887794a12375e9ecfe1e8fd54027769140abaa5445c59b251a22fb2ad36be6896")
-flag = []
-
 
 def read_until(s, token):
     """Reads from socket `s` until a string `token` is found in the response of the server"""
@@ -19,20 +17,30 @@ def char_entschluesseln(blocks, entschluesselte_werte, i):
     for char in range(0, 256):
         if char != i + 1:
 
-            angriffsvektor = [0x0] * 16
-            for j in range(0, 16):
-                if j < (15 - i):
-                    angriffsvektor[j] = entschluesselte_werte[j]
-                elif j == (15 - i):
-                    angriffsvektor[j] = entschluesselte_werte[j] ^ (i + 1) ^ char
-                else:
-                    angriffsvektor[j] = entschluesselte_werte[j] ^ (i + 1) ^ 0x00
-
             blocks_permuted = blocks[:]
             iv_permuted = iv
+
             if len(blocks) == 1:
-                iv_permuted = bytes([(iv[i] ^ (angriffsvektor[i])) for i in range(len(iv))])
+
+                for j in range(0, 16):
+                    if j < (15 - i):
+                        iv_permuted[j] = entschluesselte_werte[j] ^ iv[j]
+                    elif j == (15 - i):
+                        iv_permuted[j] = entschluesselte_werte[j] ^ (i + 1) ^ char ^ iv[j]
+                    else:
+                        iv_permuted[j] = entschluesselte_werte[j] ^ (i + 1) ^ 0x00 ^ iv[j]
+
             else:
+
+                angriffsvektor = [0x0] * 16
+                for j in range(0, 16):
+                    if j < (15 - i):
+                        angriffsvektor[j] = entschluesselte_werte[j]
+                    elif j == (15 - i):
+                        angriffsvektor[j] = entschluesselte_werte[j] ^ (i + 1) ^ char
+                    else:
+                        angriffsvektor[j] = entschluesselte_werte[j] ^ (i + 1) ^ 0x00
+
                 blocks_permuted[-2] = bytes([(blocks[-2][i] ^ (angriffsvektor[i])) for i in range(len(blocks[-2]))])
 
             s = socket.socket()
@@ -44,13 +52,14 @@ def char_entschluesseln(blocks, entschluesselte_werte, i):
 
             if b"OK" in response:
                 entschluesselte_werte[15 - i] = char
-                flag.insert(0, i + 1)
+                return char
 
     entschluesselte_werte[15 - i] = i + 1
-    flag.insert(0, i + 1)
+    return i + 1
 
 #Los
 
+flag = []
 bloecke = []
 
 for i in range(0, len(msg), 16):
@@ -64,8 +73,9 @@ for b_nr in range(0, 4):
 
     # jedes einzelne Byte entschlüsseln
     for i in range(0, 16):
-        char_entschluesseln(bloecke, entschluesselterBlock, i)
+        n = char_entschluesseln(bloecke, entschluesselterBlock, i)
         print(str(bytes(entschluesselterBlock[::-1]))[2:-1])
+        flag.insert(0, n)
 
     # hintersten block entfernen nachdem voll entschlüsselt
     del bloecke[-1]
